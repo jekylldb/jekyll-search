d5511e4ee8d27b28fdd3e118e30f308bb1059feb
jekylldb <dbecker1988@yahoo.com>
2016-08-26 17:04:54 -0500

    Update search.config.js

--- assets/search.config.js
+++ assets/search.config.js
@@ -3,6 +3,6 @@ SimpleJekyllSearch({
   resultsContainer: document.getElementById('results-container'),
   json: '/{url}/search.json',
   // searchResultTemplate: '<li><a href="{url}"><p>{title}</p></a></li>',
-  searchResultTemplate: '<li><a href="{{site.baseurl}}"><p><option>{title}</option></p></a></li>',
+  searchResultTemplate: '<li><a href="/jekyll-search/"><p><option>{title}</option></p></a></li>',
   noResultsText: 'No results found' 
 })
